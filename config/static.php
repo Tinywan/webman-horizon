6c94d1a9c71167ba026e984ce9180278f2808c46
Tinywan <tinywan@gmail.com>
2026-09-14 14:06:59 +0800

    feat: add config/static.php to enable static file serving for horizon plugin

---
+++ config/static.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @desc 应用插件静态资源配置
+ * @author Tinywan(ShaoBo Wan)
+ * @date 2026/09/14
+ */
+
+declare(strict_types=1);
+
+return [
+    'enable' => true,
+    'middleware' => [],
+];
